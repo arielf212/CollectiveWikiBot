8fa81d4c44004482589141cf1e74f797bab3fc7c
InfinityDivided <50176025+InfinityDivided@users.noreply.github.com>
2019-10-19 15:59:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,17 @@ elif action == "2":
 
     for card_name in card_names:
         for stats in stats_list:
-            if card_name == stats[1]:
+            if card_name == stats[1] and card_name not in blacklist:
                 new_stats = get_card_extra(stats)
                 update_infobox(new_stats)
+
+                upload_file(new_stats)
 
 elif action == "3":
     for stats in stats_list:
         new_stats = get_card_extra(stats)
+        upload_file(new_stats)
+
         update_infobox(new_stats)
 
 elif action == "4":
@@ -34,4 +38,14 @@ elif action == "4":
 
     for k in range(submit_count):
         upload_template(stats_list[k])
-        
+
+        sleep(5)
+
+
+elif action == "5":
+    submit_count = int(input(prompt_5))
+
+    for k in range(submit_count):
+        upload_tooltip(stats_list[k])
+
+        sleep(5)
